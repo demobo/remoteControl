1c419bb3c72596cd77f63820da37929f5287fb0f
Jeff Lin <jeff@demobo.com>
2012-08-13 02:18:34 -0700

    first check in

---
+++ rc1/static/rc/bombardtv/test.js
@@ -0,0 +1,26 @@
+var testCases = [
+		{
+			fn : "addVideoItems",
+			param : [{
+			    vid: 'EvquyiBRHCk',
+			    title: 'Super Creepy Justin Bieber \'Boyfriend\' Parody',
+			    viewCount: '222,055'
+			}, {
+			    vid: 'Z9kB2sTd8UE',
+			    title: 'Fourth of July outfit ideas!!!',
+			    viewCount: '219,540'
+			}, {
+			    vid: '8cJiBArztwA',
+			    title: '"CALL ME MAYBE" CARLY RAE JEPSEN MUSIC VIDEO PARODY',
+			    viewCount: '5,616,678'
+			}]
+		}, {
+			fn : "addCommentItems",
+			param : [{
+			    cid: 'EvquyiBRHCk',
+			    title: 'Super Creepy Justin Bieber \'Boyfriend\' Parody'
+			}, {
+			    cid: 'Z9kB2sTd8UE',
+			    title: 'Fourth of July outfit ideas!!!'
+			}]
+		} ];
---
+++ rc1/static/rc/douban/test.js
@@ -0,0 +1,12 @@
+var testCases = [
+		{
+			fn : "loadSongInfo",
+			param : {
+				albumArtUrl : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
+				songName : "Older",
+				artist : "Band Of Horses"
+			}
+		}, {
+			fn : "loadChannelList",
+			param : ["我的私人兆赫","我的红心兆赫","旅行~songtaste~ MHz","奥迪Q3驾驭简洁 MHz","咖啡 MHz","张悬城市地图 MHz","华语 MHz","古典 MHz" ]
+		} ];

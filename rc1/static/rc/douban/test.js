4f513b0d2a63ce32fa42e7c96eaedd63650a0240
Jeff Lin <jeff@demobo.com>
2012-08-16 13:19:38 -0700

    douban and pandora both use the same test.js style

--- rc1/static/rc/douban/test.js
+++ rc1/static/rc/douban/test.js
@@ -2,11 +2,28 @@ var testCases = [
 		{
 			fn : "loadSongInfo",
 			param : {
-				albumArtUrl : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
-				songName : "Older",
-				artist : "Band Of Horses"
+				image : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
+				title : "Older",
+				artist : "Band Of Horses",
+				album : "Infinite Arms - 2012/08/09"
 			}
 		}, {
 			fn : "loadChannelList",
-			param : ["我的私人兆赫","我的红心兆赫","旅行~songtaste~ MHz","奥迪Q3驾驭简洁 MHz","咖啡 MHz","张悬城市地图 MHz","华语 MHz","古典 MHz" ]
+			param : [ {
+				title : "我的私人兆赫"
+			}, {
+				title : "我的红心兆赫"
+			}, {
+				title : "旅行~songtaste~ MHz"
+			}, {
+				title : "奥迪Q3驾驭简洁 MHz"
+			}, {
+				title : "咖啡 MHz"
+			}, {
+				title : "张悬城市地图 MHz"
+			}, {
+				title : "华语 MHz"
+			}, {
+				title : "古典 MHz"
+			} ]
 		} ];

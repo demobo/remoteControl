ec07d93585f3f12444589af865cd76046460e522
Jeff Lin <jeff@demobo.com>
2012-08-29 17:41:14 -0700

    grooveshark is ready to go

--- rc1/static/apps/grooveshark.com.js
+++ rc1/static/apps/grooveshark.com.js
@@ -1,6 +1,6 @@
 function setDemoboController() {
 	demobo.setController( {
-		url : "http://rc1.demobo.com/rc/grooveshark"
+		url : "http://rc1.demobo.com/rc/grooveshark?0829"
 	});
 }
 
@@ -9,14 +9,19 @@ function demoboInitiation() {
 	// make sure the target is an element that will not be destroyed after meta
 	// data updates
 	var _this = {
-		target : jQuery('#playerDetails_nowPlaying')[0],
-		oldValue : jQuery('.queue-item.queue-item-active .albumart img').attr('src')
+		target : jQuery('#queue_list')[0],
+		oldValue : ''
 	};
 	_this.onChange = function() {
-		var newValue = jQuery('.queue-item.queue-item-active .albumart img').attr('src');
-		if (newValue && _this.oldValue !== newValue) {
+		console.log('onChange send now playing');
+		var newValue = "";
+		if (GS.player.activeSong)
+			newValue += GS.player.activeSong.SongName;
+		if (GS.player.nextSongToPlay)
+			newValue += GS.player.nextSongToPlay.SongName;
+		if (_this.oldValue !== newValue) {
 			_this.oldValue = newValue;
-			refreshController();
+			sendNowPlaying();
 		}
 	};
 	_this.delay = function() {
@@ -34,6 +39,7 @@ demoboInputDispatcher.addCommands( {
 	'spamButton' : spam,
 	'volumeSlider' : setVolume,
 	'stationItem' : chooseStation,
+	'pinBoardItem' : choosePinBoard,
 	'nowPlayingTab' : refreshController,
 	'demoboApp' : function() {
 		refreshController();
@@ -43,23 +49,27 @@ demoboInputDispatcher.addCommands( {
 
 // ********** custom event handler functions *************
 function play() {
-	jQuery('#player_play_pause.play').click();
+	// jQuery('#player_play_pause.play').click();
+	GS.player.togglePlayPause();
 }
 
 function pause() {
-	jQuery('#player_play_pause.pause').click();
+	// jQuery('#player_play_pause.pause').click();
+	GS.player.togglePlayPause();
 }
 
 function prev() {
-	jQuery('#player_previous').click();
+	// jQuery('#player_previous').click();
+	player.previousSong();
 }
 
 function next() {
-	jQuery('#player_next').click();
+	// jQuery('#player_next').click();
+	GS.player.nextSong();
 }
 
 function love() {
-	jQuery('.queue-item.queue-item-active .smile').click();
+	jQuery('.queue-item.queue-item-active .smile:not(.active)').click();
 }
 
 function spam() {
@@ -72,8 +82,8 @@ function setVolume(num) {
 }
 
 function sendNowPlaying() {
+	console.log('sendNowPlaying');
 	demobo.callFunction('loadSongInfo', getNowPlayingData());
-	demobo.callFunction('setCurrentChannel', getCurrentStationIndex());
 }
 
 function refreshController() {
@@ -84,51 +94,88 @@ function refreshController() {
 
 /* helpers */
 function getNowPlayingData() {
-	return {
-		'title' : GS.player.activeSong.SongName,
-		'artist' : GS.player.activeSong.ArtistName,
-		'album' : GS.player.activeSong.AlbumName,
-		'image' : jQuery('.queue-item.queue-item-active img').attr('src')
-	};
+	var toReturn = [];
+	if (GS.player.activeSong)
+		toReturn.push( {
+			'title' : GS.player.activeSong.SongName,
+			'artist' : GS.player.activeSong.ArtistName,
+			'album' : GS.player.activeSong.AlbumName,
+			'image' : GS.player.activeSong.getImageURL()
+		});
+	if (GS.player.nextSongToPlay)
+		toReturn.push( {
+			'title' : GS.player.nextSongToPlay.SongName,
+			'artist' : GS.player.nextSongToPlay.ArtistName,
+			'album' : GS.player.nextSongToPlay.AlbumName,
+			'image' : getImageURL(GS.player.nextSongToPlay)
+		});
+	return toReturn;
+}
+
+function getImageURL(song) {
+	var a = _.orEqual(a, 70);
+	var b = GS.Models.Song.artPath + a + "_album.png";
+	if (song.CoverArtFilename && song.CoverArtFilename.indexOf("default") == -1)
+		b = GS.Models.Song.artPath + a + "_" + song.CoverArtFilename;
+	return b
 }
 
 function getStationList() {
 	var toReturn = [];
-	jQuery('#player_radio_label').click();
-	jQuery.each(jQuery('#jjmenu_main .jj_menu_item_station .jj_menu_item_text'), function(index, elem) {
+	jQuery.each(GS.Models.Station.getStationsStartMenu(),
+			function(index, elem) {
+				var s = {
+					'title' : elem.title
+				};
+				toReturn.push(s);
+			});
+	return toReturn;
+}
+
+function getPinBoard() {
+	var toReturn = [];
+	jQuery.each(jQuery('.sidebar_link .label'), function(index, elem) {
 		var s = {
 			'title' : jQuery(elem).text().trim()
 		};
-		if (index == 0)
-			s.selected = true;
-		toReturn.push(s);
-	});
-	jQuery('#player_radio_label').click();
-	jQuery.each(jQuery('#jjmenu_main .jj_menu_item_station .jj_menu_item_text'), function(index, elem) {
-		var s = {
-			'title' : jQuery(elem).text().trim()
-		};
-		if (index == 0)
-			s.selected = true;
 		toReturn.push(s);
 	});
 	return toReturn;
 }
 
 function chooseStation(index) {
+	if (!$.isNumeric(index)) return;
 	index = parseInt(index);
-	jQuery('#player_radio_label').click();
-	jQuery(jQuery('#recentStationsList li a span')[index]).click();
-	jQuery('.btn span[data-translate-text="POPUP_START_RADIO_TITLE"]').click();
+	var stationName = GS.Models.Station.getStationsStartMenu()[index].title.toLowerCase();
+	var stationID = GS.Models.Station.getStationByName(stationName).TagID;
+	GS.player.setAutoplay(true, stationID);
+	if (jQuery('.btn span[data-translate-text="POPUP_START_RADIO_TITLE"]').length)
+		jQuery('.btn span[data-translate-text="POPUP_START_RADIO_TITLE"]')
+				.click();
 }
 
-function getCurrentStationIndex() {
-	var toReturn = 0;
-	return toReturn;
+function choosePinBoard(index) {
+	if (!$.isNumeric(index)) return;
+	index = parseInt(index);
+	var sidebarLink = jQuery(jQuery('.sidebar_link')[index]);
+	if (sidebarLink.hasClass('artist') || sidebarLink.hasClass('playlist')) {
+		sidebarLink.contextmenu();
+		setTimeout(function() {
+			jQuery('.jj_menu_item_play').click();
+		}, 100);
+	} else {
+		sidebarLink.click();
+	}
+	if (jQuery('.btn span[data-translate-text="POPUP_START_RADIO_TITLE"]').length)
+		jQuery('.btn span[data-translate-text="POPUP_START_RADIO_TITLE"]').click();
+	setTimeout(function() {
+		GS.player.setAutoplay(true);
+	}, 2000);
 }
 
 function sendStationList() {
 	demobo.callFunction('loadChannelList', getStationList());
+	demobo.callFunction('loadPinBoard', getPinBoard());
 }
 
 function setDevice(data) {
--- rc1/static/rc/grooveshark/test.js
+++ rc1/static/rc/grooveshark/test.js
@@ -1,12 +1,17 @@
 var testCases = [
 		{
 			fn : "loadSongInfo",
-			param : {
+			param : [{
 				image : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
 				title : "Older",
 				artist : "Band Of Horses",
 				album : "Infinite Arms - 2012/08/09"
-			}
+			},{
+				image : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
+				title : "Another Song",
+				artist : "Akon",
+				album : "Infinite Arms - 2012/08/09"
+			}]
 		}, {
 			fn : "loadChannelList",
 			param : [ {
@@ -14,8 +19,7 @@ var testCases = [
 			}, {
 				title : "我的红心兆赫"
 			}, {
-				title : "旅行~songtaste~ MHz",
-				selected : true
+				title : "旅行~songtaste~ MHz"
 			}, {
 				title : "奥迪Q3驾驭简洁 MHz"
 			}, {
@@ -27,8 +31,23 @@ var testCases = [
 			}, {
 				title : "古典 MHz"
 			} ]
-		},
-		{
-			fn : "setCurrentChannel",
-			param : 1
+		}, {
+			fn : "loadPinBoard",
+			param : [ {
+				title : "我的私人兆赫"
+			}, {
+				title : "我的红心兆赫"
+			}, {
+				title : "旅行~songtaste~ MHz"
+			}, {
+				title : "奥迪Q3驾驭简洁 MHz"
+			}, {
+				title : "咖啡 MHz"
+			}, {
+				title : "张悬城市地图 MHz"
+			}, {
+				title : "华语 MHz"
+			}, {
+				title : "古典 MHz"
+			} ]
 		}];

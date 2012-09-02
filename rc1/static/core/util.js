6a61bed57ae669b137c1f29402741d4fc7a838c9
Jiahao Li <lijiahao90@gmail.com>
2012-09-01 17:06:15 -0700

    bookmarklet js migration

---
+++ rc1/static/core/util.js
@@ -0,0 +1,59 @@
+function getCurrentDomain() {
+	return document.domain ? document.domain : window.location.hostname;
+}
+
+function showDemobo() {
+	var demoboCover = document.createElement('div');
+	demoboCover.setAttribute('id', 'demoboCover');
+	demoboCover.style.zIndex = '9999';
+	demoboCover.style.textAlign = 'center';
+	demoboCover.style.position = 'fixed';
+	demoboCover.style.width = '100%';
+	demoboCover.style.height = '100%';
+	demoboCover.style.top = '0';
+	demoboCover.style.left = '0';
+	demoboCover.style.backgroundColor = 'rgba(0,0,0,0.6)';
+
+	document.body.appendChild(demoboCover);
+
+	demobo.renderQR('demoboCover', 1);
+	var temp = document.querySelector('#demoboCover > img');
+	temp.style.position = 'relative';
+	temp.style.top = '30%';
+}
+
+function hideDemobo() {
+	var temp = document.getElementById('demoboCover');
+	if (temp) {
+		temp.parentNode.removeChild(temp);
+	}
+}
+
+function toggleDemobo() {
+	var a = document.getElementById('demoboCover');
+	if (a) {
+		hideDemobo();
+	} else {
+		showDemobo();
+	}
+}
+
+function demoboCustomInputDispatcher() {
+	console.log("util demoboCustomInputDispatcher");
+	this.acceptableCommands = {};
+	this.addCommand = function(command, handler) {
+		this.acceptableCommands[command] = handler;
+	};
+	this.addCommands = function(dict) {
+		for ( var command in dict) {
+			this.acceptableCommands[command] = dict[command];
+		}
+	};
+	this.execCommand = function(command, data) {
+		if (command in this.acceptableCommands) {
+			this.acceptableCommands[command](data);
+		}
+	};
+}
+
+var demoboInputDispatcher = new demoboCustomInputDispatcher();

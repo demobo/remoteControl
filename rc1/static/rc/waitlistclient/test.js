c443bfdd082a975a9c1498eb57f3c3c09d3a90df
Jeff Lin <jeff@demobo.com>
2013-03-03 23:28:44 -0800

    added notify

--- rc1/static/rc/waitlistclient/test.js
+++ rc1/static/rc/waitlistclient/test.js
@@ -7,7 +7,7 @@ var testCases1 = [
 var testCases2 = [
         {
 			functionName : "loadQueue",
-			data : {name: 'smallQueue', list: [{name:'Jeff',number:10,id:54,time:'6:30PM'},{name:'Lap',number:4,id:56, notes:'No smoking'},{name:'Larry',number:2,id:57}]}
+			data : {name: 'smallQueue', list: [{name:'Jeff',number:10,id:54,time:'6:30PM',status:'notified'},{name:'Lap',number:4,id:56, notes:'No smoking'},{name:'Larry',number:2,id:57}]}
 		}];
 
 var testSuite = [testCases1,testCases2];

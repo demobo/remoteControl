1f142aba19dc7cbbd5eef0cf3f49fde0a2ed0db5
Jiahao Li <lijiahao90@gmail.com>
2013-05-29 15:10:40 -0700

    insertTextAtCursor inserts only by triggering 'textInput' event

--- rc1/static/apps/inputtool.js
+++ rc1/static/apps/inputtool.js
@@ -34,28 +34,28 @@
 	
 	function insertTextAtCursor(text) {
 		element = document.activeElement;
-		if (document.selection) {
-	        element.focus();
-	        var sel = document.selection.createRange();
-	        sel.text = text;
-	        element.focus();
-	    } else if (element.selectionStart || element.selectionStart === 0) {
-	        var startPos = element.selectionStart;
-	        var endPos = element.selectionEnd;
-	        var scrollTop = element.scrollTop;
-	        element.value = element.value.substring(0, startPos) + text + element.value.substring(endPos, element.value.length);
-	        element.focus();
-	        element.selectionStart = startPos + text.length;
-	        element.selectionEnd = startPos + text.length;
-	        element.scrollTop = scrollTop;
-	    } else if (element.value!=undefined){
-	        element.value += text;
-	        element.focus();
-	    } else{
+//		if (document.selection) {
+//	        element.focus();
+//	        var sel = document.selection.createRange();
+//	        sel.text = text;
+//	        element.focus();
+//	    } else if (element.selectionStart || element.selectionStart === 0) {
+//	        var startPos = element.selectionStart;
+//	        var endPos = element.selectionEnd;
+//	        var scrollTop = element.scrollTop;
+//	        element.value = element.value.substring(0, startPos) + text + element.value.substring(endPos, element.value.length);
+//	        element.focus();
+//	        element.selectionStart = startPos + text.length;
+//	        element.selectionEnd = startPos + text.length;
+//	        element.scrollTop = scrollTop;
+//	    } else if (element.value!=undefined){
+//	        element.value += text;
+//	        element.focus();
+//	    } else{
           var e = document.createEvent('TextEvent');
           e.initTextEvent('textInput', true, true, null, text, 'zh-CN');
           element.dispatchEvent(e);
           element.focus();
-      }
+//      }
 	}
 //})();

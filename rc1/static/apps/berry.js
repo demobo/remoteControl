a9cfaa7a120592cb7883e9cc489184d0bc981003
Lap Chan <lapchan@gmail.com>
2013-07-13 02:00:15 -0700

    add firebase library

--- rc1/static/apps/berry.js
+++ rc1/static/apps/berry.js
@@ -4,36 +4,57 @@
 		version : '0130'
 	};
 	demoboBody.injectScript('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', function() {
-		jQuery.noConflict();
-		if (DEMOBO) {
-			DEMOBO.autoConnect = true;
-			DEMOBO.init = init;
-			demobo.start();
-		}
-		demoboLoading = undefined;
-
-		ui.controllerUrl = "http://rc1.demobo.com/rc/" + ui.name + "?" + ui.version;
-
-		// do all the iniations you need here
-		function init() {
-			demobo._sendToSimulator('setData', {
-				key : 'url',
-				value : location.href
-			});
-			demobo.setController({
-				url : ui.controllerUrl,
-				orientation : 'portrait'
-			});
-			// your custom demobo input event dispatcher
-			demobo.mapInputEvents({
-				'demoboApp' : onReady,
-			});
-		}
-
-		// ********** custom event handler functions *************
-		function onReady() {
-
-		}
-
+	  demoboBody.injectScript('https://cdn.firebase.com/v0/firebase.js', function(){ 
+	  
+  		jQuery.noConflict();
+  		if (DEMOBO) {
+  			DEMOBO.autoConnect = true;
+  			DEMOBO.init = init;
+  			demobo.start();
+  		}
+  		demoboLoading = undefined;
+  
+  		ui.controllerUrl = "http://rc1.demobo.com/rc/" + ui.name + "?" + ui.version;
+  
+  		// do all the iniations you need here
+  		function init() {
+  			demobo._sendToSimulator('setData', {
+  				key : 'url',
+  				value : location.href
+  			});
+  			demobo.setController({
+  				url : ui.controllerUrl,
+  				orientation : 'portrait'
+  			});
+  			// your custom demobo input event dispatcher
+  			demobo.mapInputEvents({
+  				'demoboApp' : onReady,
+  				'outgoingCall' : outgoingCall
+   			});
+   			
+   			var incomingId = demobo_guid;
+        var incomingCallRef = new Firebase('https://de-berry.firebaseio-demo.com/' + incomingId);
+        incomingCallRef.on('child_added', function(snapshot) {
+          var message = snapshot.val();
+          displayChatMessage(message.name, message.text);
+        });
+  		}
+  
+  		// ********** custom event handler functions *************
+  		function onReady() {
+  
+  		}
+  		
+  		function outgoingCall(outgoingId) {
+  		  var outgoingCallRef = new Firebase('https://de-berry.firebaseio-demo.com/' + outgoingId);
+  		  outgoingCallRef.push({name: demobo_guid, text: "calling"});
+  		}
+  
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
+      
+    });
 	});
 })();

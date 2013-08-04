8226c0d91f2af7f4ae07fb3fe3bcf9649a7ca0e0
Jiahao Li <lijiahao90@gmail.com>
2013-08-04 09:07:09 -0700

    added light.js

--- rc1/static/apps/light.js
+++ rc1/static/apps/light.js
@@ -6,53 +6,55 @@
 
 	demoboBody.injectScript('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', function() {
 		demoboBody.injectScript('https://cdn.firebase.com/v0/firebase.js', function() {
-			jQuery.noConflict();
-			if (DEMOBO) {
-				DEMOBO.autoConnect = true;
-				DEMOBO.init = init;
-				demobo.start();
-			}
-			demoboLoading = undefined;
+      demoboBody.injectScript('http://localhost:1240/dev/LightConsole.js', function(){
+			  jQuery.noConflict();
+			  if (DEMOBO) {
+			  	DEMOBO.autoConnect = true;
+			  	DEMOBO.init = init;
+			  	demobo.start();
+			  }
+			  demoboLoading = undefined;
 
-			ui.controllerUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "/control.html?" + ui.version;
-			ui.incomingCallCtrlUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "incoming" + "/control.html?" + ui.version;
+			  ui.controllerUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "/control.html?" + ui.version;
+			  ui.incomingCallCtrlUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "incoming" + "/control.html?" + ui.version;
 
-			// do all the iniations you need here
-			function init() {
-				toggleLiveInput();
-				demobo.setController({
-					url : ui.controllerUrl,
-					orientation: "portrait"
-				});
-				demobo.setController({
-					'page' : 'wheel'
-				}, "1FC0071B-44A9-4091-B3E7-32083D4DB5B6");
-				
-				// your custom demobo input event dispatcher
-				demobo.mapInputEvents({
-					'demoboApp' : onReady,
-				});
-				demobo.addEventListener("update", function(e) {
-					console.log(e.x, e.y, e.z)
-				});
-				
-				// var items = ["red", "blue", "white", "green", "pink", "yellow", "magenta"];
-				// setInterval(function() {
-					// demobo.callFunction("changeBackgroundColor", {
-						// rgb : items[Math.floor(Math.random()*items.length)]
-					// });
-					// demobo.callFunction("changeForegroundColor", {
-						// rgb : items[Math.floor(Math.random()*items.length)]
-					// })
-				// }, 1000);
-			}
+			  // do all the iniations you need here
+			  function init() {
+			  	toggleLiveInput();
+			  	demobo.setController({
+			  		url : ui.controllerUrl,
+			  		orientation: "portrait"
+			  	});
+			  	demobo.setController({
+			  		'page' : 'wheel'
+			  	}, "1FC0071B-44A9-4091-B3E7-32083D4DB5B6");
+			  	
+			  	// your custom demobo input event dispatcher
+			  	demobo.mapInputEvents({
+			  		'demoboApp' : onReady,
+			  	});
+			  	demobo.addEventListener("update", function(e) {
+			  		console.log(e.x, e.y, e.z)
+			  	});
+			  	
+			  	// var items = ["red", "blue", "white", "green", "pink", "yellow", "magenta"];
+			  	// setInterval(function() {
+			  		// demobo.callFunction("changeBackgroundColor", {
+			  			// rgb : items[Math.floor(Math.random()*items.length)]
+			  		// });
+			  		// demobo.callFunction("changeForegroundColor", {
+			  			// rgb : items[Math.floor(Math.random()*items.length)]
+			  		// })
+			  	// }, 1000);
+          window.lc = new window.LightConsole();
+			  }
 
-			// ********** custom event handler functions *************
-			function onReady() {
-				demobo.callFunction('IncomingCallStatus', {
-				});
-			}
-
+			  // ********** custom event handler functions *************
+			  function onReady() {
+			  	demobo.callFunction('IncomingCallStatus', {
+			  	});
+			  }
+      });
 		});
 	});
 })();

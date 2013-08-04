5f0ebb28e29980f2162162129fc0f3b3ff596032
Jeff Lin <JeffLin@Jeffs-MacBook-Pro.local>
2013-08-04 03:37:44 -0700

    added frequency dectection

--- rc1/static/v1/momos/lightcontrol/detector/js/pitchdetect.js
+++ rc1/static/v1/momos/lightcontrol/detector/js/pitchdetect.js
@@ -1,159 +1,3 @@
-(function() {
-	var ui = {
-		name : 'light2',
-		version : '0712'
-	};
-
-	demoboBody.injectScript('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', function() {
-		demoboBody.injectScript('https://cdn.firebase.com/v0/firebase.js', function() {
-			jQuery.noConflict();
-			if (DEMOBO) {
-				DEMOBO.autoConnect = true;
-				DEMOBO.init = init;
-				demobo.start();
-			}
-			demoboLoading = undefined;
-
-			ui.controllerUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "/control.html?" + ui.version;
-			ui.incomingCallCtrlUrl = "http://rc1.demobo.com/v1/momos/" + ui.name + "incoming" + "/control.html?" + ui.version;
-
-			// do all the iniations you need here
-			function init() {
-				toggleLiveInput();
-				demobo.setController({
-					url : ui.controllerUrl,
-				});
-				demobo.setController({
-					'page' : 'wheel'
-				}, "1FC0071B-44A9-4091-B3E7-32083D4DB5B6");
-				// your custom demobo input event dispatcher
-				demobo.mapInputEvents({
-					'demoboApp' : onReady,
-				});
-				demobo.addEventListener("update", function(e) {
-					console.log(e.x, e.y, e.z)
-				});
-				
-				// var items = ["red", "blue", "white", "green", "pink", "yellow", "magenta"];
-				// setInterval(function() {
-					// demobo.callFunction("changeBackgroundColor", {
-						// rgb : items[Math.floor(Math.random()*items.length)]
-					// });
-					// demobo.callFunction("changeForegroundColor", {
-						// rgb : items[Math.floor(Math.random()*items.length)]
-					// })
-				// }, 1000);
-			}
-
-			// ********** custom event handler functions *************
-			function onReady() {
-				demobo.callFunction('IncomingCallStatus', {
-				});
-			}
-
-		});
-	});
-})();
-
-
-
-
-
-
-(function (global, exports, perf) {
-  'use strict';
-
-  function fixSetTarget(param) {
-    if (!param)	// if NYI, just return
-      return;
-    if (!param.setTargetAtTime)
-      param.setTargetAtTime = param.setTargetValueAtTime; 
-  }
-
-  if (window.hasOwnProperty('webkitAudioContext') && 
-      !window.hasOwnProperty('AudioContext')) {
-    window.AudioContext = webkitAudioContext;
-
-    AudioContext.prototype.internal_createGain = AudioContext.prototype.createGain;
-    AudioContext.prototype.createGain = function() { 
-      var node = this.internal_createGain();
-      fixSetTarget(node.gain);
-      return node;
-    };
-
-    AudioContext.prototype.internal_createDelay = AudioContext.prototype.createDelay;
-    AudioContext.prototype.createDelay = function() { 
-      var node = this.internal_createDelay();
-      fixSetTarget(node.delayTime);
-      return node;
-    };
-
-    AudioContext.prototype.internal_createBufferSource = AudioContext.prototype.createBufferSource;
-    AudioContext.prototype.createBufferSource = function() { 
-      var node = this.internal_createBufferSource();
-      if (!node.start) {
-        node.start = function ( when, offset, duration ) {
-          if ( offset || duration )
-            this.noteGrainOn( when, offset, duration );
-          else
-            this.noteOn( when );
-        }
-      }
-      if (!node.stop)
-        node.stop = node.noteoff;
-      fixSetTarget(node.playbackRate);
-      return node;
-    };
-
-    AudioContext.prototype.internal_createDynamicsCompressor = AudioContext.prototype.createDynamicsCompressor;
-    AudioContext.prototype.createDynamicsCompressor = function() { 
-      var node = this.internal_createDynamicsCompressor();
-      fixSetTarget(node.threshold);
-      fixSetTarget(node.knee);
-      fixSetTarget(node.ratio);
-      fixSetTarget(node.reduction);
-      fixSetTarget(node.attack);
-      fixSetTarget(node.release);
-      return node;
-    };
-
-    AudioContext.prototype.internal_createBiquadFilter = AudioContext.prototype.createBiquadFilter;
-    AudioContext.prototype.createBiquadFilter = function() { 
-      var node = this.internal_createBiquadFilter();
-      fixSetTarget(node.frequency);
-      fixSetTarget(node.detune);
-      fixSetTarget(node.Q);
-      fixSetTarget(node.gain);
-      return node;
-    };
-
-    if (AudioContext.prototype.hasOwnProperty( 'createOscillator' )) {
-      AudioContext.prototype.internal_createOscillator = AudioContext.prototype.createOscillator;
-      AudioContext.prototype.createOscillator = function() { 
-        var node = this.internal_createOscillator();
-        if (!node.start)
-          node.start = node.noteOn; 
-        if (!node.stop)
-          node.stop = node.noteOff;
-        fixSetTarget(node.frequency);
-        fixSetTarget(node.detune);
-        return node;
-      };
-    }
-
-    if (!AudioContext.prototype.hasOwnProperty('createGain'))
-      AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
-    if (!AudioContext.prototype.hasOwnProperty('createDelay'))
-      AudioContext.prototype.createDelay = AudioContext.prototype.createDelayNode;
-    if (!AudioContext.prototype.hasOwnProperty('createScriptProcessor'))
-      AudioContext.prototype.createScriptProcessor = AudioContext.prototype.createJavaScriptNode;
-  }
-}(window));
-
-
-
-
-
 var audioContext = new AudioContext();
 var isPlaying = false;
 var sourceNode = null;
@@ -167,6 +11,15 @@ var detectorElem,
 	detuneAmount;
 
 window.onload = function() {
+	// var request = new XMLHttpRequest();
+	// request.open("GET", "../sounds/whistling3.ogg", true);
+	// request.responseType = "arraybuffer";
+	// request.onload = function() {
+	  // audioContext.decodeAudioData( request.response, function(buffer) { 
+	    	// theBuffer = buffer;
+		// } );
+	// }
+	// request.send();
 
 	detectorElem = document.getElementById( "detector" );
 	canvasElem = document.getElementById( "output" );
@@ -174,6 +27,31 @@ window.onload = function() {
 	noteElem = document.getElementById( "note" );
 	detuneElem = document.getElementById( "detune" );
 	detuneAmount = document.getElementById( "detune_amt" );
+
+	// detectorElem.ondragenter = function () { 
+		// this.classList.add("droptarget"); 
+		// return false; };
+	// detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+	// detectorElem.ondrop = function (e) {
+  		// this.classList.remove("droptarget");
+  		// e.preventDefault();
+		// theBuffer = null;
+// 
+	  	// var reader = new FileReader();
+	  	// reader.onload = function (event) {
+	  		// audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		// theBuffer = buffer;
+	  		// }, function(){alert("error loading!");} ); 
+// 
+	  	// };
+	  	// reader.onerror = function (event) {
+	  		// alert("Error: " + reader.error );
+		// };
+	  	// reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	// return false;
+	// };
+
+
 
 }
 
@@ -361,18 +239,6 @@ function updatePitch( time ) {
 		" - note: " + noteFromPitch( pitch ) +
 		" - confidence: " + confidence + "% "
 		);
-		var note =  noteFromPitch( pitch );
-		demobo.callFunction("syncState", {
-			isPlaying:true,
-			curPower:(note%12)/12,
-			oldPower:confidence/100*5,
-		});
-		demobo.callFunction("changeBackgroundColor", {
-			rgb : colors[note%12]
-		});
-		demobo.callFunction("changeForegroundColor", {
-			rgb : colors[note%12]
-		});
 	 	// pitchElem.innerText = Math.floor( pitch );
 	 	// var note =  noteFromPitch( pitch );
 		// noteElem.innerText = noteStrings[note%12];
@@ -394,5 +260,3 @@ function updatePitch( time ) {
 		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
 	rafID = window.requestAnimationFrame( updatePitch );
 }
-
-

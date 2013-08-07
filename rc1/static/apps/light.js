c9aa5db4227e5529f5cf27866d5af77c5966114c
Jeff Lin <JeffLin@Jeffs-MacBook-Pro.local>
2013-08-07 00:21:14 -0700

    added dlc.setPattern

--- rc1/static/apps/light.js
+++ rc1/static/apps/light.js
@@ -149,8 +149,9 @@
         	if (!curState || !stateEnable) return;
 			demobo.callFunction("syncState", curState);
 			if (window.syncState) syncState(curState);
+			// dlc.setPattern(curState.pattern);
+			dlc.setGobo(0); //setGobo takes one param of value 0-14, of which 1-14 corresponds to 14 gobo shapes in goboShapes.bmp in Dropbox folder, and 0 is the default circle shape. Specifically, Index 1-7 are rotating gobos and index 8-14 are static gobos. 
 			dlc.setColor(curColor);
-      dlc.setGobo(0); //setGobo takes one param of value 0-14, of which 1-14 corresponds to 14 gobo shapes in goboShapes.bmp in Dropbox folder, and 0 is the default circle shape. Specifically, Index 1-7 are rotating gobos and index 8-14 are static gobos. 
        		dlc.setDMX();
        		stateEnable = false;
        		setTimeout(function(){

f969b1e70aba7100ffe90821ed3e51dbc212f2e5
Jeff Lin <JeffLin@Jeffs-MacBook-Pro.local>
2013-08-07 17:34:27 -0700

    fixed nowrap

--- rc1/static/apps/light.js
+++ rc1/static/apps/light.js
@@ -73,7 +73,7 @@
                         var curleft= parseInt(e.value.left)*dpiAdjust;
                         var prevTop = parseInt(e.value.prevTop)*dpiAdjust;
                         var prevLeft= parseInt(e.value.prevLeft)*dpiAdjust;
-                        var target = $(e.value.html).css('position', 'absolute').css('font-size', '33px');
+                        var target = $(e.value.html).css('position', 'absolute').css('font-size', '33px').css('white-space', 'nowrap');
 
                         console.log(target);
                         $('#iphoneDockScreen').append(target);

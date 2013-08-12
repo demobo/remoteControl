3214d8a9db90efae710a5954e8c99352728c076f
Lap Chan <lapchan@gmail.com>
2013-08-12 01:40:16 -0700

    put momo page for communication bobo

--- rc1/static/v1/momos/communicationtool/test.js
+++ rc1/static/v1/momos/communicationtool/test.js
@@ -4,6 +4,11 @@ var testCases1 = [
 			data : 
 			  [{data:"(415) 982-7877","type":"telephone"}]
 		}];
-
-var testSuite = [testCases1];
-// var testSuite = [testCases1,testCases2];
+var testCases2 = [
+    {
+      functionName : "onReceiveData",
+      data : 
+        [{data:"(415) 982-1234","type":"telephone"}]
+    }];
+//var testSuite = [testCases1];
+var testSuite = [testCases1,testCases2];

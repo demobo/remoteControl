b45ad201d64dec812f536f4a42364c80d6be4d66
Jiahao Li <lijiahao90@gmail.com>
2013-08-12 15:29:30 -0700

    modifed dev ext to support portal upgrade

--- extension/demobo-dev-ext/js/main.js
+++ extension/demobo-dev-ext/js/main.js
@@ -13,16 +13,16 @@ var setBobos = {
 if (!document.getElementById('toggle')){
   var toggle = document.createElement('div');
   if (dev){
-    toggle.setAttribute('onclick', 'javascript:((function(c){c.demoboAddBobos='+JSON.stringify(addBobos)+';c._extension=1;var d=window.__dmtg;((!c.demoboPortal&&function(){var a=new Date,b=c.document.createElement("script"),e=(document.location.protocol==="https:")?"https://localhost:443":"http://localhost:'+appEnginePort+'";window.demoboBase=e;b.src=e+"/core/entry.js?"+a.getTime();b.className="demoboJS";c.document.body&&c.document.body.appendChild(b)})||d)()})(window))');
+    toggle.setAttribute('onclick', 'javascript:((function(c){(c.demoboPortal&&c.demoboPortal.set("mode","EXTENSION"));c.demoboAddBobos='+JSON.stringify(addBobos)+';c._extension=1;var d=window.__dmtg;((!c.demoboPortal&&function(){var a=new Date,b=c.document.createElement("script"),e=(document.location.protocol==="https:")?"https://localhost:443":"http://localhost:'+appEnginePort+'";window.demoboBase=e;b.src=e+"/core/entry.js?"+a.getTime();b.className="demoboJS";c.document.body&&c.document.body.appendChild(b)})||d)()})(window))');
   }else{
-    toggle.setAttribute('onclick', 'javascript:((function(c){c.demoboAddBobos='+JSON.stringify(addBobos)+';c._extension=1;var d=window.__dmtg;((!c.demoboPortal&&function(){var a=new Date,b=c.document.createElement("script"),e="//d1hew6xzj9n4kw.cloudfront.net";window.demoboBase=e;b.src="//d32q09dnclw46p.cloudfront.net/entry.js?"+a.getTime();b.className="demoboJS";c.document.body&&c.document.body.appendChild(b)})||d)()})(window))'); 
+    toggle.setAttribute('onclick', 'javascript:((function(c){(c.demoboPortal&&c.demoboPortal.set("mode","EXTENSION"));c.demoboAddBobos='+JSON.stringify(addBobos)+';c._extension=1;var d=window.__dmtg;((!c.demoboPortal&&function(){var a=new Date,b=c.document.createElement("script"),e="//d1hew6xzj9n4kw.cloudfront.net";window.demoboBase=e;b.src="//d32q09dnclw46p.cloudfront.net/entry.js?"+a.getTime();b.className="demoboJS";c.document.body&&c.document.body.appendChild(b)})||d)()})(window))'); 
   }
   toggle.setAttribute('id', 'toggle');
   document.body.appendChild(toggle);
 
   var load = document.createElement('div');
   load.setAttribute('id', 'load');
-  load.setAttribute('onclick', 'javascript:(function(){if (window.demobo)return;document.getElementById("toggle").click()})()');
+  load.setAttribute('onclick', 'javascript:(function(){document.getElementById("toggle").click()})()');
   document.body.appendChild(load);
 
   function onMessage(message, sender, sendResponse) {

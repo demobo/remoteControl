6747df814951c4b61ebfd0fd7609f346ef0b5ac2
Jiahao Li <lijiahao90@gmail.com>
2013-08-25 13:00:20 -0700

    added sear controller

---
+++ rc1/static/v1/momos/sear/main.js
@@ -0,0 +1,13 @@
+/* setting */
+var registeredplayers = Array();
+var players = Array(1,2);
+var deviceID;
+var oldPower = 0;
+var olderPower = 0;
+var curPower = 0;
+var olderPeakPower = 0;
+var oldPeakPower = 0;
+var curPeakPower = 0;
+var gR=20;
+var gG=20;
+var gB=20;
---
+++ rc1/static/v1/momos/sear/test.js
@@ -0,0 +1,57 @@
+function rnd(start, end){
+    return Math.floor(Math.random() * (end - start) + start);
+}
+
+var testCases1 = [
+		{
+			functionName : "syncState",
+			data : {
+				isPlaying:true,
+				curPower:Math.random()*1.2,
+				oldPower:Math.random(),
+			}
+		}];
+
+var testCases2 = [
+		{
+			functionName : "loadSongInfo",
+			data : {
+				image : "http://cont-sv5-3.pandora.com/images/public/amz/7/2/0/1/886976911027_500W_500H.jpg",
+				title : "Older",
+				artist : "Band Of Horses",
+				album : "Infinite Arms - 2012/08/09"
+			}
+		}];
+
+var testCases3 = [{
+	functionName : "syncState",
+	data : {
+		isPlaying : true,
+		curPower : Math.random() * 1.2,
+		oldPower : Math.random(),
+		color : {
+			r : rnd(0, 255),
+			g : rnd(0, 255),
+			b : rnd(0, 255),
+		},
+		pattern : rnd(1,50),
+	}
+}];
+
+var testCases4 = [{
+	functionName : "syncState",
+	data : {
+		isPlaying : true,
+		curPower : Math.random() * 1.2,
+		oldPower : Math.random(),
+		color : {
+			r : rnd(0, 255),
+			g : rnd(0, 255),
+			b : rnd(0, 255),
+		},
+		pattern : rnd(1,50),
+	}
+}];
+
+var testSuite = [testCases1,testCases2,testCases3,testCases4];
+// var testSuite = [testCases1,testCases2];

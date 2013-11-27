b280103fed5953e67adf6c0f46901c203c49d47f
Jeff Lin <JeffLin@Jeffs-MacBook-Pro.local>
2013-11-27 12:13:20 -0800

    colabeo

--- rc2/static/apps/generic.js
+++ rc2/static/apps/generic.js
@@ -23,27 +23,37 @@
 			var evtData = e.detail.data.data;
 			switch(evtData.action) {
 				case "sync":
-					demobo._sendToSimulator('event', {
-						url : window.location.href,
-						action : "urlChange"
-					});
+					if (top === self) {
+						demobo._sendToSimulator('event', {
+							url : window.location.href,
+							action : "urlChange"
+						});
+					}
 					break;
 				case "urlChange":
+					if (top !== self)
+						return;
 					if (window.location.href == evtData.url)
 						return;
 					window.location = evtData.url;
 					break;
 				case "click":
-					if (evtData.index >= 0)
-						var dom = jQuery(evtData.selector)[evtData.index];
-					else
-						var dom = jQuery(evtData.selector);
+					if (window.jQuery) {
+						if (evtData.index >= 0)
+							var dom = jQuery(evtData.selector)[evtData.index];
+						else
+							var dom = jQuery(evtData.selector);
+					} else {
+						if (evtData.index >= 0)
+							var dom = document.getElementsByClassName(evtData.selector)[evtData.index];
+						else
+							var dom = document.getElementsByClassName(evtData.selector);
+					}
 					dom.click();
 					break;
 				default:
 			}
 		});
-
 
 		function onClickByClassName(className) {
 			jQuery(document).on("mouseup", className, function(e) {
@@ -56,14 +66,53 @@
 				console.log("click", className, index);
 			});
 		}
+
+		function setUpListenerByClassName(delegatorClassName, elementClassName, eventName) {
+			var delegator = document.getElementsByClassName(delegatorClassName)[0];
+			setUpListener(delegator, elementClassName, eventName);
+		}
+
+		function setUpListener(delegator, elementClassName, eventName) {
+			delegator.addEventListener(eventName, function(event) {
+				event = event || window.event;
+				var target = event.target || event.srcElement;
+				while (target && target != delegator) {
+					if (target && target.classList.contains(elementClassName)) {
+						var targets = Array.prototype.slice.call(document.getElementsByClassName(elementClassName));
+						console.log(elementClassName, targets, target);
+						var index = Array.prototype.indexOf.call(targets, target);
+						// demobo._sendToSimulator('event', {
+							// selector : elementClassName,
+							// index : index,
+							// action : eventName
+						// });
+						console.log("click", elementClassName, index);
+						break;
+					}
+					target = target.parentNode;
+				}
+			});
+		}
 		
+		function a(elementClassName) {
+			var targets = window.document.getElementsByClassName(elementClassName);
+			console.log(elementClassName, targets);
+		}
+
 		if (window.jQuery) {
 			onClickByClassName('.btnNext');
 			onClickByClassName('.btnPrevious');
+
 			onClickByClassName('.btn.next');
 			onClickByClassName('.btn.prev');
+
 			onClickByClassName('.list-card');
 			onClickByClassName('.icon-close');
+
+			onClickByClassName('.filename-col img');
+			onClickByClassName('.filename-col a');
+		} else {
+			setUpListenerByClassName('clsDesktopActionTabWrapper', 'clsDesktopActionTab', 'click');
 		}
 	};
 

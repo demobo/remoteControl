07405e8d08d757d01762acd876fd1beb28bc8e3d
jeffcolabeo <jeff@colabeo.com>
2014-06-02 22:23:25 -0700

    iframe bug

--- rc1/static/rc/famous/src/main.js
+++ rc1/static/rc/famous/src/main.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
+    var Utility = require('famous/utilities/Utility');
     var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
@@ -8,22 +9,33 @@ define(function(require, exports, module) {
     var Scrollview = require('famous/views/Scrollview');
     var StateModifier = require('famous/modifiers/StateModifier');
     var ViewSequence = require('famous/core/ViewSequence');
-    var Easing = require('famous/transitions/Easing');
+    var View = require('famous/core/View');
+	var EventHandler = require('famous/core/EventHandler');
+	
+	var Easing = require('famous/transitions/Easing');
     var Transitionable = require('famous/transitions/Transitionable');
     var SpringTransition = require('famous/transitions/SpringTransition');
-    var View = require('famous/core/View');
-    var SequentialLayout = require("famous/views/SequentialLayout");
-    var HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
-	var GridLayout = require("famous/views/GridLayout");
-	var EventHandler = require('famous/core/EventHandler');
+    var WallTransition = require("famous/transitions/WallTransition");
+    var TweenTransition = require("famous/transitions/TweenTransition");
+	var SnapTransition = require("famous/transitions/SnapTransition");
+	var TransitionableTransform = require("famous/transitions/TransitionableTransform");
+
 	
 	var MouseSync     = require("famous/inputs/MouseSync");
 	var TouchSync     = require("famous/inputs/TouchSync");
 	var ScrollSync    = require("famous/inputs/ScrollSync");
 	var GenericSync   = require("famous/inputs/GenericSync");
-	var SnapTransition = require("famous/transitions/SnapTransition");
 	
 	var Lightbox      = require('famous/views/Lightbox');
+	var RenderController = require("famous/views/RenderController");
+	
+	var SequentialLayout = require("famous/views/SequentialLayout");
+    var HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
+	var GridLayout = require("famous/views/GridLayout");
+	var Deck           = require('famous/views/Deck');
+	var Flipper    = require("famous/views/Flipper");
+	var FlexibleLayout = require('famous/views/FlexibleLayout');
+	var EdgeSwapper = require("famous/views/EdgeSwapper");
 
     var mainContext = Engine.createContext();
 
--- rc1/static/v1/bobos/famo.us.js
+++ rc1/static/v1/bobos/famo.us.js
@@ -1,7 +1,18 @@
 (function() {
   	var Famous = window.Bobo.extend();
+  	
+  	var logoUrl = 'http://famo.us/images/famous_symbol.svg';
+  	var controllerUrl = 'http://rc1.demobo.com/rc/famous?'+Math.random();
+	var counter = 0;
+	var codeText;
+	var controller = {
+		url : controllerUrl + counter,
+		orientation: 'portrait'
+	};
+	var onEditorChange = debounce(_onEditorChange,300);
 	
   	Famous.prototype.initialize = function(){
+  		
 	    this.setInfo('priority', 2);
 	    this.setInfo('iconClass', 'fui-play-circle');
 	    this.setInfo('boboID', 'famous');
@@ -20,28 +31,12 @@
 			'demoboApp':   'onReady'
 		});
 		
-		var controllerUrl = 'http://rc1.demobo.com/rc/famous?'+Math.random();
-		var counter = 0;
-		var codeText;
-		var controller = {
-			url : controllerUrl + counter,
-			orientation: 'portrait'
-		};
 		this.setController( controller );
-		if (typeof ace !== 'undefined' && $('#editor-view')[0]) {
-			this.editor = ace.edit('editor-view');
-			if (this.editor) {
-				var onChange = debounce(_onChange.bind(this),300);
-				function _onChange(e) {	
-					console.log(1);
-					if (codeText==this.editor.getValue()) return;
-					console.log(2);
-					codeText=this.editor.getValue();
-					counter = (counter+1)%2;
-					controller.url = controllerUrl + counter;
-					demobo.setController( controller );
-				}
-				this.editor.on('change', onChange.bind(this));
+		if (typeof ace !== 'undefined') {
+			if ($('#editor-view')[0]) {
+				_setupEditorEvents.call(this);
+			} else {
+				
 			}
 		} else {
 			$('body')[0].addEventListener ('DOMSubtreeModified', 
@@ -55,21 +50,45 @@
 
 	// ********** custom event handler functions *************
 	Famous.prototype.onReady = function () {
-		if (this.editor) {
-			var code = "function render() { " + this.editor.getValue() + " }";
-			demobo.callFunction("eval", code);
-			demobo.callFunction("render","");
-			console.log(3);
+		if (typeof ace !== 'undefined') {
+			if (this.editor) {
+				var code = "function runFamous() { " + this.editor.getValue() + " }; runFamous();";
+				demobo.callFunction("eval", code);
+				console.log(3);
+			} else 
+				demobo.setPage({url: logoUrl, touchEnabled: false});
 		} else {
 			_onDemoChange.call(this);
 		}
 	};
 
+
+	function _setupEditorEvents(e) {	
+		if (this.editor) {
+			this.editor.off('change', onEditorChange.bind(this));
+		}
+		this.editor = ace.edit('editor-view');
+		if (this.editor) {
+			this.editor.on('change', onEditorChange.bind(this));
+		}
+	}
+	
+	function _onEditorChange(e) {	
+		console.log(1);
+		if (codeText==this.editor.getValue()) return;
+		console.log(2);
+		codeText=this.editor.getValue();
+		counter = (counter+1)%2;
+		controller.url = controllerUrl + counter;
+		demobo.setController( controller );
+	}
 	function _onDemoChange() {
 		setTimeout(function(){
 			var iframeSrc = $('iframe:visible').attr('src');
-			if (iframeSrc) {
+			if (iframeSrc && iframeSrc.indexOf('api.demobo.com')==-1) {
 				demobo.setPage({url: iframeSrc, touchEnabled: true});	
+			} else {
+				demobo.setPage({url: logoUrl, touchEnabled: false});
 			}
 		},300);
 	}

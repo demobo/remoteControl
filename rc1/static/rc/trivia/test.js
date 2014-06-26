7723b06bd205695c4b4990bc5b5e32ae46e1a3c7
jeffcolabeo <jeff@colabeo.com>
2014-06-25 17:38:06 -0700

    supported true or false

--- rc1/static/rc/trivia/test.js
+++ rc1/static/rc/trivia/test.js
@@ -1,19 +1,25 @@
-var testCases1 = [
+var testCases0 = [
 		{
 			functionName : "renderAnswers",
-			data : ["A very long answer and others","B","c","D"]
+			data : ["True","False","",""]
 		}];
 
-var testCases2 = [
+var testCases1 = [
          		{
          			functionName : "renderBuzzer",
          			data : []
          		}];
 
+var testCases2 = [
+		{
+			functionName : "renderAnswers",
+			data : ["A very long answer and others","B","c","D"]
+		}];
+		
 var testCases3 = [
            		{
            			functionName : "renderFillintheblank",
            			data : []
            		}];
 
-var testSuite = [testCases1,testCases2,testCases3];
+var testSuite = [testCases0, testCases1,testCases2,testCases3];
